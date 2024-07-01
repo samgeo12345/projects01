00ee05a9440c0450abafd1865e79fbe1db46467c
samgeo12345 <samgeoj12d@gmail.com>
2024-07-01 15:59:40 +0530

    portfolio

--- index.js
+++ index.js
@@ -10,3 +10,18 @@ document.addEventListener("DOMContentLoaded",function(){
     }
     setInterval(change,1500);
 });
+document.addEventListener("DOMContentLoaded",function(){
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const target = document.querySelector(this.getAttribute('href'));
+            if (target) {
+                window.scrollTo({
+                    top: target.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
